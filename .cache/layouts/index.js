1c3d69922857277c895cac66c795bdde29e3d327
Tim Upchurch <mail@timup.ch>
2018-09-01 16:52:52 -0400

    remove unecessary files from git tracking

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/tim/Desktop/timandco/raleighbeach/src/layouts/index.js"
-  import data from "/Users/tim/Desktop/timandco/raleighbeach/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
--- public/path----b4f4cc9c98e2ff37e2d1.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Raleigh Beach"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----b4f4cc9c98e2ff37e2d1.js.map
