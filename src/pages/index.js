0e9992e019cc2eab272b5df0c3ea03f08dbbaaf1
Tim Upchurch <mail@timup.ch>
2018-09-04 09:12:27 -0400

    add emoji to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    {/* <h1>Yo people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link> */}
+   <p>⛱</p>
   </div>
 )
 
