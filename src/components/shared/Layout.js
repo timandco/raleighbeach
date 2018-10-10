605b66ef7c800363d6272f6ca1a4b085ffda2157
Tim Upchurch <mail@timup.ch>
2018-10-10 17:49:55 -0400

    clean up layout component, remove unused funcs

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'react-emotion';
-import { push } from 'gatsby';
-// import StoreContext, { defaultStoreContext } from '../../context/StoreContext';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
@@ -9,98 +7,46 @@ import { StaticQuery, graphql } from 'gatsby';
 import Header from '../header';
 import './layout.css';
 
-// const Layout = ({ children }) => (
-//   <StaticQuery
-//     query={graphql`
-//       query SiteTitleQuery {
-//         site {
-//           siteMetadata {
-//             title
-//           }
-//         }
-//       }
-//     `}
-//     render={data => (
-//       <>
-//         <Helmet
-//           title={data.site.siteMetadata.title}
-//           meta={[
-//             { name: 'description', content: 'Sample' },
-//             { name: 'keywords', content: 'sample, something' }
-//           ]}
-//         >
-//           <html lang="en" />
-//         </Helmet>
-//         <Header siteTitle={data.site.siteMetadata.title} />
-//         <div
-//           style={{
-//             margin: '0 auto',
-//             maxWidth: 960,
-//             padding: '0px 1.0875rem 1.45rem',
-//             paddingTop: 0
-//           }}
-//         >
-//           {children}
-//         </div>
-//       </>
-//     )}
-//   />
-// );
-
-// Layout.propTypes = {
-//   children: PropTypes.node.isRequired
-// };
-
-// export default Layout;
-
-export default class Layout extends React.Component {
-  // state = {
-  //   store: {
-  //     ...defaultStoreContext
-  //   }
-  // }
-
-  render() {
-    return (
-
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
           }
-        `}
-        render={data => (
-          <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Sample' },
-                { name: 'keywords', content: 'sample, something' }
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <div
-              style={{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '0px 1.0875rem 1.45rem',
-                paddingTop: 0
-              }}
-            >
-              {this.props.children}
-            </div>
-          </>
-        )}
-      />
-        
-          
-     
-    );
-  }
-}
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' }
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
